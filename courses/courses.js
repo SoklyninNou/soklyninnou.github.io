9c29078a0d744a2f2a48b2d438f3f172b21fa58f
Soklynin Nou <sly.nou@berkeley.edu>
2025-08-15 12:02:35 -0700

    added filters

--- courses/courses.js
+++ courses/courses.js
@@ -1,12 +1,65 @@
-function toggleTeaching() {
-    const legend = document.getElementById('teaching');
-    const teachingCourses = document.querySelectorAll('.item#teaching');
-    const firstItem = document.querySelector('.item#teaching');
+function toggleHonors() {
+    const legend = document.getElementById('honors-legend');
+    const teachingCourses = document.querySelectorAll('.honors');
 
-    legend.classList.toggle('show');
-    
-    firstItem.classList.toggle('show');
+    legend.classList.toggle('off');
+
     teachingCourses.forEach(item => {
-        item.classList.toggle('show');
+        item.classList.toggle('hide');
+    });
+}
+
+function toggleMaths() {
+    const legend = document.getElementById('maths-legend');
+    const teachingCourses = document.querySelectorAll('.maths');
+
+    legend.classList.toggle('off');
+
+    teachingCourses.forEach(item => {
+        item.classList.toggle('hide');
+    });
+}
+
+function toggleCompsci() {
+    const legend = document.getElementById('compsci-legend');
+    const teachingCourses = document.querySelectorAll('.compsci');
+
+    legend.classList.toggle('off');
+
+    teachingCourses.forEach(item => {
+        item.classList.toggle('hide');
+    });
+}
+
+function togglePhysics() {
+    const legend = document.getElementById('physics-legend');
+    const teachingCourses = document.querySelectorAll('.physics');
+
+    legend.classList.toggle('off');
+
+    teachingCourses.forEach(item => {
+        item.classList.toggle('hide');
+    });
+}
+
+function toggleTeaching() {
+    const legend = document.getElementById('teaching-legend');
+    const teachingCourses = document.querySelectorAll('.teaching');
+
+    legend.classList.toggle('off');
+
+    teachingCourses.forEach(item => {
+        item.classList.toggle('hide');
+    });
+}
+
+function toggleElectives() {
+    const legend = document.getElementById('electives-legend');
+    const teachingCourses = document.querySelectorAll('.electives');
+
+    legend.classList.toggle('off');
+
+    teachingCourses.forEach(item => {
+        item.classList.toggle('hide');
     });
 }
