1855348a1dd350f7d27aea003ce6de6b5e8a1524
Soklynin Nou <sly.nou@berkeley.edu>
2025-08-17 21:03:44 -0700

    reduced courses.js redundancies

--- courses/courses.js
+++ courses/courses.js
@@ -1,65 +1,10 @@
-function toggleHonors() {
-    const legend = document.getElementById('honors-legend');
-    const teachingCourses = document.querySelectorAll('.honors');
+function toggle(className) {
+    const legend = document.getElementById(className + '-legend');
+    const course = document.querySelectorAll(className);
 
     legend.classList.toggle('off');
 
-    teachingCourses.forEach(item => {
-        item.classList.toggle('hide');
-    });
-}
-
-function toggleMaths() {
-    const legend = document.getElementById('maths-legend');
-    const teachingCourses = document.querySelectorAll('.maths');
-
-    legend.classList.toggle('off');
-
-    teachingCourses.forEach(item => {
-        item.classList.toggle('hide');
-    });
-}
-
-function toggleCompsci() {
-    const legend = document.getElementById('compsci-legend');
-    const teachingCourses = document.querySelectorAll('.compsci');
-
-    legend.classList.toggle('off');
-
-    teachingCourses.forEach(item => {
-        item.classList.toggle('hide');
-    });
-}
-
-function togglePhysics() {
-    const legend = document.getElementById('physics-legend');
-    const teachingCourses = document.querySelectorAll('.physics');
-
-    legend.classList.toggle('off');
-
-    teachingCourses.forEach(item => {
-        item.classList.toggle('hide');
-    });
-}
-
-function toggleTeaching() {
-    const legend = document.getElementById('teaching-legend');
-    const teachingCourses = document.querySelectorAll('.teaching');
-
-    legend.classList.toggle('off');
-
-    teachingCourses.forEach(item => {
-        item.classList.toggle('hide');
-    });
-}
-
-function toggleElectives() {
-    const legend = document.getElementById('electives-legend');
-    const teachingCourses = document.querySelectorAll('.electives');
-
-    legend.classList.toggle('off');
-
-    teachingCourses.forEach(item => {
+    course.forEach(item => {
         item.classList.toggle('hide');
     });
 }
